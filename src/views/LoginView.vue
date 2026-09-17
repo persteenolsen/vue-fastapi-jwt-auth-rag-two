c0d0152c7f91732c1d555ce80dd9491fa660e888
Per Olsen <persteen22@gmail.com>
2026-09-17 08:24:07 +0200

    Tested the build and deployment workflow

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,7 +23,7 @@ function onSubmit(values, { setErrors }) {
         <h3>Chat Assistant by FastAPI, Vue, RAG & AI</h3>
         
         <br/><br />
-        Last updated: 10-09-2026<br /><br />
+        Last updated: 17-09-2026<br /><br />
         
         <h2>Login</h2>
         <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
